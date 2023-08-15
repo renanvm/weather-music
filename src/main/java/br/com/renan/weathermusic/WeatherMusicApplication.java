a25aa2f7dd87c74063b73a0ae417e9a758aa3a40
Renan Martins <renan@renan>
2023-08-15 14:43:34 -0300

    first commit

---
+++ src/main/java/br/com/renan/weathermusic/WeatherMusicApplication.java
@@ -0,0 +1,13 @@
+package br.com.renan.weathermusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/renan/weathermusic/WeatherMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.renan.weathermusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
